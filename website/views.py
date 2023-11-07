0efc522832690de5a0095decee37231e772e74f2
dorstrauss <dorshtrauss11@gmail.com>
2023-11-07 15:11:36 +0200

    build streaming as explained in D-ID

--- website/views.py
+++ website/views.py
@@ -1,5 +1,12 @@
+import os
+
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['DID_API'] = {'url': 'https://api.d-id.com', 'key': os.environ.get('D-ID_API_KEY')}
+        return context
