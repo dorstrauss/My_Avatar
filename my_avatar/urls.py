e3638b589063bb0c1ab4786855808eba0c8c6356
dorstrauss <dorshtrauss11@gmail.com>
2023-11-16 21:34:13 +0200

    now the welcome video is played in the beginning

--- my_avatar/urls.py
+++ my_avatar/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from website.views import HomeView, chatbot
+from my_avatar import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('chatbot/', chatbot, name='chatbot'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
