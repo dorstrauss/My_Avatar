932360180e5392f289ffbf6296ddcf9dececada7
dorstrauss <dorshtrauss11@gmail.com>
2023-11-23 20:56:49 +0200

    added a favicon to the website

--- my_avatar/urls.py
+++ my_avatar/urls.py
@@ -3,9 +3,12 @@ from django.urls import path
 from website.views import HomeView, chatbot
 from my_avatar import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomeView.as_view(), name='home'),
     path('chatbot/', chatbot, name='chatbot'),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico'))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
