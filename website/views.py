f802f287ffc43987ddc22b84df53318319c8c19e
dorstrauss <dorshtrauss11@gmail.com>
2023-12-05 18:13:57 +0200

    changed the D-ID API key to a valid enviroment name

--- website/views.py
+++ website/views.py
@@ -11,7 +11,7 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['DID_API'] = {'url': 'https://api.d-id.com', 'key': os.environ.get('D-ID_API_KEY')}
+        context['DID_API'] = {'url': 'https://api.d-id.com', 'key': os.environ.get('D_ID_API_KEY')}
         return context
 
 
